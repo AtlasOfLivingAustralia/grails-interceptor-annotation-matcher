937e840b47ab5705439116c5b39de0c79a8b1c1c
Simon Bear <simon.bear@csiro.au>
2022-02-18 18:42:51 +1100

    Extract AnnotationResult to separate class and fix access rights

--- src/main/java/au/org/ala/grails/AnnotationMatcher.java
+++ src/main/java/au/org/ala/grails/AnnotationMatcher.java
@@ -188,31 +188,4 @@ public class AnnotationMatcher {
                 );
     }
 
-    static class AnnotationResult<T extends Annotation, U extends Annotation> {
-        public final T controllerAnnotation;
-        public final T actionAnnotation;
-        public final U overrideAnnotation;
-
-        AnnotationResult() {
-            controllerAnnotation = null;
-            actionAnnotation = null;
-            overrideAnnotation = null;
-        }
-
-        AnnotationResult(T controllerAnnotation, T actionAnnotation, U overrideAnnotation) {
-            this.controllerAnnotation = controllerAnnotation;
-            this.actionAnnotation = actionAnnotation;
-            this.overrideAnnotation = overrideAnnotation;
-        }
-
-        /**
-         * Returns the annotation that would apply for the action, either the action annotation or the controller's
-         * annotation if the action didn't have one.  The override annotation is not considered in this method.
-         * @return The effective annotation for this result
-         */
-        public T effectiveAnnotation() {
-            return actionAnnotation != null ? actionAnnotation : controllerAnnotation;
-        }
-    }
-
 }
